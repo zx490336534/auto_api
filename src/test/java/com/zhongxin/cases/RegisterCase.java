3fe97d6bd5140d1ffa1dbf032c3de7af2f1eb181
钟鑫 <490336534@qq.com>
2020-08-04 07:24:05 +0800

    将json格式修改form格式

--- src/test/java/com/zhongxin/cases/RegisterCase.java
+++ src/test/java/com/zhongxin/cases/RegisterCase.java
@@ -1,6 +1,7 @@
 package com.zhongxin.cases;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
+import com.alibaba.fastjson.JSONObject;
 import com.zhongxin.pojo.CaseInfo;
 import com.zhongxin.utils.ExcelUtils;
 import com.zhongxin.utils.HttpUtils;
@@ -9,6 +10,7 @@ import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 注册接口测试类型
@@ -20,18 +22,30 @@ public class RegisterCase {
         try {
             HashMap<String, String> headers = new HashMap<>();
             headers.put("X-Lemonban-Media-Type", "lemonban.v1");
+
+            String params = caseInfo.getParams();
+            String url = caseInfo.getUrl();
+            String method = caseInfo.getMethod();
             String contentType = caseInfo.getContentType();
+
             if ("json".equals(contentType)) {
                 headers.put("Content-Type", "application/json");
             } else if ("form".equals(contentType)) {
+                Map<String, String> map = JSONObject.parseObject(params, Map.class);
+                String formParams = "";
+                for (String key : map.keySet()) {
+                    formParams += key + "=" + map.get(key) + "&";
+                }
+                params = formParams.substring(0, formParams.length() - 1);
                 headers.put("Content-Type", "application/x-www-form-urlencoded");
             }
-            if ("post".equals(caseInfo.getMethod())) {
-                HttpUtils.post(caseInfo.getUrl(), caseInfo.getParams(), headers);
-            } else if ("get".equals(caseInfo.getMethod())) {
-                HttpUtils.get(caseInfo.getUrl(), headers);
-            } else if ("patch".equals(caseInfo.getMethod())) {
-                HttpUtils.patch(caseInfo.getUrl(), caseInfo.getParams(), headers);
+
+            if ("post".equals(method)) {
+                HttpUtils.post(url, params, headers);
+            } else if ("get".equals(method)) {
+                HttpUtils.get(url, headers);
+            } else if ("patch".equals(method)) {
+                HttpUtils.patch(url, params, headers);
             }
         } catch (Exception e) {
             e.printStackTrace();
