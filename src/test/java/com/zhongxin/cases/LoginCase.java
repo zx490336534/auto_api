ee6da5809c98396373ec29a8b6a3980bcde5d172
钟鑫 <490336534@qq.com>
2020-08-06 06:47:31 +0800

    参数化2

--- src/test/java/com/zhongxin/cases/LoginCase.java
+++ src/test/java/com/zhongxin/cases/LoginCase.java
@@ -3,12 +3,22 @@ package com.zhongxin.cases;
 import com.zhongxin.pojo.CaseInfo;
 import com.zhongxin.utils.ExcelUtils;
 import com.zhongxin.utils.HttpUtils;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class LoginCase {
+    public int sheetIndex;
+
+    @BeforeClass
+    @Parameters({"sheetIndex"})
+    public void beforeClass(int sheetIndex) {
+        this.sheetIndex = sheetIndex;
+    }
+
     @Test(dataProvider = "datas")
     public void test(CaseInfo caseInfo) {
         HttpUtils.call(caseInfo);
@@ -16,7 +26,7 @@ public class LoginCase {
 
     @DataProvider
     public Object[] datas() {
-        List list = ExcelUtils.read(1, 1, CaseInfo.class);
+        List list = ExcelUtils.read(this.sheetIndex, 1, CaseInfo.class);
         return list.toArray();
     }
 }
--- src/test/java/com/zhongxin/cases/RegisterCase.java
+++ src/test/java/com/zhongxin/cases/RegisterCase.java
@@ -5,7 +5,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.zhongxin.pojo.CaseInfo;
 import com.zhongxin.utils.ExcelUtils;
 import com.zhongxin.utils.HttpUtils;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -16,6 +18,13 @@ import java.util.Map;
  * 注册接口测试类型
  */
 public class RegisterCase {
+    public int sheetIndex;
+
+    @BeforeClass
+    @Parameters({"sheetIndex"})
+    public void beforeClass(int sheetIndex) {
+        this.sheetIndex = sheetIndex;
+    }
 
     @Test(dataProvider = "datas")
     public void test(CaseInfo caseInfo) {
@@ -24,7 +33,7 @@ public class RegisterCase {
 
     @DataProvider
     public Object[] datas() {
-        List list = ExcelUtils.read(0, 1, CaseInfo.class);
+        List list = ExcelUtils.read(this.sheetIndex, 1, CaseInfo.class);
         return list.toArray();
     }
 }
