651e4b42113c64f247fe664bb90a402f44821d0c
钟鑫 <490336534@qq.com>
2020-08-15 09:00:23 +0800

    测试管理员用户信息

---
+++ src/test/java/com/zhongxin/cases/UserInfoCase.java
@@ -0,0 +1,35 @@
+package com.zhongxin.cases;
+
+import com.zhongxin.pojo.CaseInfo;
+import com.zhongxin.utils.Constants;
+import com.zhongxin.utils.ExcelUtils;
+import com.zhongxin.utils.HttpUtils;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 用户信息接口测试
+ */
+public class UserInfoCase extends BaseCase {
+
+    @Test(dataProvider = "datas")
+    public void test(CaseInfo caseInfo) {
+        paramsReplace(caseInfo);
+        HashMap<String, String> headers = getAuthorizationHeader();
+        String responseBody = HttpUtils.call(caseInfo, headers);
+        boolean responseAssertFlag = responseAssert(caseInfo.getExpectedResult(), responseBody);
+        String assertResult = responseAssertFlag ? "PASSED" : "FAILED";
+        addWriteBackData(sheetIndex, caseInfo.getId(), Constants.RESPONSE_CELL_NUM, responseBody);
+        addWriteBackData(sheetIndex, caseInfo.getId(), Constants.ASSERT_CELL_NUM, assertResult);
+    }
+
+
+    @DataProvider
+    public Object[] datas() {
+        List list = ExcelUtils.read(this.sheetIndex, 1, CaseInfo.class);
+        return list.toArray();
+    }
+}
